d845deda942a6689b98e37676c7f7cbca8f5d474
Jamie Conway <jamie@jpconway.com>
2023-07-11 16:14:57 -0500

    update navitems

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,15 +1,16 @@
 import NavItem from "./NavItem";
 
 const NavLinks = () => {
-    const mainNavItems = ['About', 'Posts', 'Skills', 'Links'];
+    const mainNavItems = [['About', '/about'], ['Posts', '/posts'], ['Skills', '/skills'], ['Links', '/links']];
+
     return (
-    <span>
-        <nav className="flex-10 flex flex-row justify-end gap-4 text-xl mr-4 text-[#2F2F30]">
-            {mainNavItems.map(mainNavItem => (
-                <NavItem key={mainNavItem} title={mainNavItem} />
-            ))}
-        </nav>
-    </span>
+        <span>
+            <nav className="flex-10 flex flex-row justify-end gap-4 text-xl mr-4 text-[#2F2F30]">
+                {mainNavItems.map(([ title, url ]) => (
+                    <NavItem key={title} title={title} url={url} />
+                ))}
+            </nav>
+        </span>
     );
 };
 

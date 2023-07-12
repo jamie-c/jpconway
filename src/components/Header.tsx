375f260e43190ab4ca4afd5f819153a1b776ca65
Jamie Conway <jamie@jpconway.com>
2023-07-12 15:00:52 -0500

    add shadow effect on scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Logo from "./Logo"
 import NavLinksHorizontal from "./NavLinks"
 import HamburgerIcon from "./icons/HamburgerIcon"
@@ -9,8 +9,8 @@ import HamburgerIcon from "./icons/HamburgerIcon"
 const Header = () => {
 
     const headerClassNames = {
-        navLinksHidden: "sticky top-0 sm:relative w-screen z-40 bg-opacity-60 bg-white backdrop-filter backdrop-blur-lg",
-        navLinksVisible: "fixed top-0 sm:relative w-screen h-screen z-40 bg-opacity-60 bg-white backdrop-filter backdrop-blur-lg"
+        navLinksHidden: "sticky top-0 sm:relative w-screen z-40 bg-opacity-60 bg-white backdrop-filter backdrop-blur-lg transition box-shadow ease",
+        navLinksVisible: "fixed top-0 sm:relative w-screen h-screen z-40 bg-opacity-60 bg-white backdrop-filter backdrop-blur-lg transition box-shadow ease"
     }
 
     const [ toggleHam, setToggleHam ] = useState(false)
@@ -26,8 +26,23 @@ const Header = () => {
         }
     }
 
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+        const isScrolled = window.scrollY > 0;
+        setScrolled(isScrolled);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className={headerClass}>
+        <header className={scrolled ? `drop-shadow-xl ${headerClass}` : headerClass}>
             <div className="px-7 flex flex-row items-center justify-between max-w-7xl h-16 m-auto ">
                 <Logo txtSize="text-2xl" bold={true} />
                 <span className="hidden sm:flex">

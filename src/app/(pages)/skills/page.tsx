b6cd98d166946df1ca3de4ddbbbba6bb996a6246
Jamie Conway <jamie@jpconway.com>
2023-07-12 10:59:58 -0500

    create skills placeholder

---
+++ src/app/(pages)/skills/page.tsx
@@ -0,0 +1,12 @@
+import HeadingOne from "@/components/HeadingOne"
+
+const Skills = () => {
+    return (
+        <>
+            <HeadingOne title="My Skills" />
+            <p>(coming soon...)</p>
+        </>
+    )
+}
+
+export default Skills

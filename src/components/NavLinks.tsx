fcb2749ce20068fb99f0c8ed9aa2efa161cb34db
Jamie Conway <jamie@jpconway.com>
2023-07-11 23:34:26 -0500

    fix nav links style

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -5,7 +5,7 @@ const NavLinks = () => {
 
     return (
         <span>
-            <nav className="flex-10 flex flex-row justify-end gap-4 text-xl mr-4 text-[#2F2F30]">
+            <nav className="flex-10 flex flex-row justify-end gap-4 text-xl text-[#2F2F30]">
                 {mainNavItems.map(([ title, url ]) => (
                     <NavItem key={title} title={title} url={url} />
                 ))}

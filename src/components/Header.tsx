885bb3129504f9f36608b62cef37975045fca145
Jamie Conway <jamie@jpconway.com>
2023-07-12 13:32:03 -0500

    add hamburgericon to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
+import HamburgerIcon from "./icons/HamburgerIcon";
 
 
 const Header = () => {
@@ -8,6 +9,9 @@ const Header = () => {
             <div className="px-7 flex flex-row items-center justify-between max-w-7xl h-16 m-auto ">
                 <Logo txtSize="text-2xl" bold={true} />
                 <NavLinks />
+                <span className="flex sm:hidden flex-row items-center h-16 justify-center m-0 overflow-hidden absolute w-12 right-4 top-0">
+                    <HamburgerIcon />
+                </span>
             </div>
         </header>
     )

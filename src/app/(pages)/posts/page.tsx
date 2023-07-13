bec2b8668c11a6fd0f3a17330c0efab1568177bc
Jamie Conway <jamie@jpconway.com>
2023-07-12 22:28:42 -0500

    add display posts info

--- src/app/(pages)/posts/page.tsx
+++ src/app/(pages)/posts/page.tsx
@@ -1,10 +1,43 @@
 import HeadingOne from "@/components/HeadingOne"
+import { getSortedPostsData } from "../../../lib/posts"
+import * as dayjs from 'dayjs'
 
-const Posts = () => {
+export async function getData() {
+    const allPostsData = getSortedPostsData()
+    if (!allPostsData) {
+        throw new Error
+    }
+    console.log("🚀 ~ file: page.tsx:14 ~ Posts ~ allPostsData:", allPostsData)
+    return allPostsData
+}
+
+const Posts = async () => {
+
+    const data = await getData()
+    console.log("🚀 ~ file: page.tsx:16 ~ Posts ~ data:", data)
+
     return (
         <>
             <HeadingOne title="My Posts" />
-            <p>(coming soon...)</p>
+            {/* Add this <section> tag below the existing <section> tag */}
+            <section >
+                <h2 >Blog</h2>
+                <ul >
+                    <li>Testing postsData</li>
+                    
+                    {data.map(({ id, date, title }) => {
+                        const year = dayjs(date).year()
+                        return (
+                        <li key={id}>
+                            {title}
+                            <br />
+                            {id}
+                            <br />
+                            {year}
+                        </li>
+                    )})}
+                </ul>
+            </section>
         </>
     )
 }

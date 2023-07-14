30455c7b7d92fed65b1155e18493d6658864e1aa
Jamie Conway <jamie@jpconway.com>
2023-07-13 23:46:05 -0500

    update post year

--- src/components/postComponents/PostYear.tsx
+++ src/components/postComponents/PostYear.tsx
@@ -1,10 +1,10 @@
 import dayjs from "dayjs"
 
-const PostYear = ({ date }: { date: string}) => {
+const PostYear = ({ date, id }: { date: string, id: string }) => {
     const year = dayjs(date).year()
 
     return (
-        <div className="text-[#8A51AE] text-4xl font-medium">
+        <div id={id} className="text-[#8A51AE] text-4xl font-medium pb-6">
             {year}
         </div>
     )

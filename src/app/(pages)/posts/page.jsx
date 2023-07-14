068299a21c1b8e590619cb9a887b905c48e347ca
Jamie Conway <jamie@jpconway.com>
2023-07-13 23:47:12 -0500

    format posts for posts page

--- src/app/(pages)/posts/page.jsx
+++ src/app/(pages)/posts/page.jsx
@@ -3,6 +3,8 @@ import { getSortedPostsData } from "../../../lib/posts"
 import dayjs from 'dayjs'
 import PostDate from "@/components/postComponents/PostDate"
 import PostYear from "@/components/postComponents/PostYear"
+import StyledLink from "@/components/StyledLink"
+import { groupCollapsed } from "console"
 
 async function getData() {
     const allPostsData = getSortedPostsData()
@@ -12,35 +14,68 @@ async function getData() {
     return allPostsData
 }
 
+
 const Posts = async () => {
 
     const data = await getData()
+    const mappedData = data.map(({ id, title, date }) => {
+        const year = dayjs(date).year()
+        const month = dayjs(date).format('YYYY-MM')
+  
+        return {
+            date,
+            year,
+            month,
+            id,
+            title
+        }
+    })
+
+    const groupedPosts = mappedData.reduce((acc, { year, month, id, date, title }) => {
+        const key = month
+        
+        if (!acc[year]) {
+          acc[year] = {};
+        }
+
+        if (!acc[year][key]) {
+            acc[year][key] = []
+        }
+
+        acc[year][key].push({id, title, date});
+
+        return acc;
+    }, {});
+    
+    const years = Object.keys(groupedPosts)
+    const getMonths = (y) => {
+        return Object.keys(groupedPosts[y])
+    }
+    
 
     return (
-        <div id="about" className="px-8 md:px-24 lg:px-48 max-w-7xl">
+        <div id="posts" className="px-8 md:px-24 lg:px-48 max-w-7xl w-full flex flex-col items-start">
             <HeadingOne title="Posts" />
             <section id="posts">
-                <ul >
-                    <li>Testing postsData</li>
-                    
-                    {data.map(({ id, date, title }) => {
-                        const year = dayjs(date).year()
-                        const month = dayjs(date).month()
-                        const day = dayjs(date).day()
-                        const dateFormatted = dayjs(date).format('YYYY-MM-DD')
-                        
-                        return (
-                        <li key={id}>
-                            <PostYear date={date} />
-                            <br />
-                            {title}
-                            <br />
-                            {id}
-                            <br />
-                            <PostDate date={date} />
-                        </li>
-                    )})}
-                </ul>
+                {years.map(year => (
+                    <>
+                    <PostYear id={year} key={year} date={year} />
+                    {getMonths(year).map(month => (
+                        <div id={month} className="pb-4" key={year}>
+                        {groupedPosts[year][month].map(({ id, date, title }) => {
+                            return (
+                                <div className="flex flex-row gap-5 pb-2" id={month} key={id}>
+                                    <span className="w-24">
+                                        <PostDate date={date} />
+                                    </span>
+                                    <StyledLink url={`/posts/${id}`} title={title} />
+                                </div>
+                            )
+                        })}
+                        </div>
+                    ))}
+                </>
+                    ))}
             </section>
         </div>
     )

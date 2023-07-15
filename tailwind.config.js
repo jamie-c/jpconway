342d270bbee2131ea97cfdb6ea5c372b30db33d5
Jamie Conway <jamie@jpconway.com>
2023-07-15 16:12:25 -0500

    define custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,52 @@ export const theme = {
       'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
     },
   },
+  colors: {
+    pureWhite: {
+          100: "#fbfbfa",
+          200: "#f8f7f5",
+          300: "#f4f4f0",
+          400: "#f1f0eb",
+          500: "#edece6",
+          600: "#bebdb8",
+          700: "#8e8e8a",
+          800: "#5f5e5c",
+          900: "#2f2f2e"
+    },
+    tricornBlack: {
+          100: "#d5d5d6",
+          200: "#acacac",
+          300: "#828283",
+          400: "#595959",
+          500: "#2f2f30",
+          600: "#262626",
+          700: "#1c1c1d",
+          800: "#131313",
+          900: "#09090a"
+    },
+    brightBlue: {
+          100: "#d8e7ff",
+          200: "#b0cfff",
+          300: "#89b6ff",
+          400: "#619eff",
+          500: "#3a86ff",
+          600: "#2e6bcc",
+          700: "#235099",
+          800: "#173666",
+          900: "#0c1b33"
+    },
+    darkBlue: {
+          100: "#cde1ff",
+          200: "#9cc2ff",
+          300: "#6aa4ff",
+          400: "#3985ff",
+          500: "#0767ff",
+          600: "#0652cc",
+          700: "#043e99",
+          800: "#032966",
+          900: "#011533"
+},
+
+  }
 };
 export const plugins = [];

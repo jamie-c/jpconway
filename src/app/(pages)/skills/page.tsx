d6194863fb8662bf8f98a1a0168173fd3761e006
Jamie Conway <jamie@jpconway.com>
2023-07-17 16:00:07 -0500

    add more skills

--- src/app/(pages)/skills/page.tsx
+++ src/app/(pages)/skills/page.tsx
@@ -9,18 +9,53 @@ const Skills = () => {
             title: 'Responsive Web Design - Free Code Camp',
             linkTo: 'https://www.freecodecamp.org/fcc6ebd6462-e213-40c0-8eaf-e06f336c09cd',
             fileName: 'ResponsiveWebDesignCert-FreeCodeCamp.jpg',
+            profileTitle: 'Free Code Camp Profile',
+            profileUrl: 'https://www.freecodecamp.org/fcc6ebd6462-e213-40c0-8eaf-e06f336c09cd',
         },
         {
             title: 'JavaScript Algorithms and Data Structures - Free Code Camp',
             linkTo: 'https://www.freecodecamp.org/fcc6ebd6462-e213-40c0-8eaf-e06f336c09cd',
             fileName: 'JavaScriptAlgorithmsAndDataStructuresCert-FreeCodeCamp.jpg',
+            profileTitle: 'Free Code Camp Profile',
+            profileUrl: 'https://www.freecodecamp.org/fcc6ebd6462-e213-40c0-8eaf-e06f336c09cd',
         },
+        {
+            title: 'Neo4j Fundamentals',
+            linkTo: 'https://graphacademy.neo4j.com/c/10df8afa-bb29-46dc-b82d-7695bb977c01/',
+            fileName: 'Neo4j-Fundamentals.png',
+            profileTitle: 'Neo4j Profile',
+            profileUrl: 'https://graphacademy.neo4j.com/u/1489cff7-f57c-4978-a0c6-3ba3e95ec411/',
+        },
+        {
+            title: 'Cypher Fundamentals',
+            linkTo: 'https://graphacademy.neo4j.com/c/2882cf20-b15e-43a4-8a1b-807d948ad524/',
+            fileName: 'Neo4j-Cypher-Fundamentals.png',
+            profileTitle: 'Neo4j Profile',
+            profileUrl: 'https://graphacademy.neo4j.com/u/1489cff7-f57c-4978-a0c6-3ba3e95ec411/',
+        },
+        {
+            title: 'Graph Data Modeling Fundamentals',
+            linkTo: 'https://graphacademy.neo4j.com/c/db658771-03ff-4413-ac22-caafb23ce57c/',
+            fileName: 'Neo4j-GDM-Fundamentals.png',
+            profileTitle: 'Neo4j Profile',
+            profileUrl: 'https://graphacademy.neo4j.com/u/1489cff7-f57c-4978-a0c6-3ba3e95ec411/',
+        },
+        ,
+        {
+            title: 'Building Neo4j Applications with Python',
+            linkTo: 'https://graphacademy.neo4j.com/c/fea8db94-1399-4e4c-9dd6-d4ccf8faea23/',
+            fileName: 'Neo4j-building-with-python.png',
+            profileTitle: 'Neo4j Profile',
+            profileUrl: 'https://graphacademy.neo4j.com/u/1489cff7-f57c-4978-a0c6-3ba3e95ec411/',
+        }
     ]
 
     return (
         <div id="skills-page" className="px-8 md:px-24 lg:px-48 max-w-7xl w-full flex flex-col items-start">
             <HeadingOne title="Skills" />
-            {skillImages.map(({ title, linkTo, fileName }: {title: string, linkTo: string, fileName: string}) => (
+            {skillImages.map(({ title, linkTo, fileName, profileTitle, profileUrl }: 
+                {title: string, linkTo: string, fileName: string, profileTitle: string, profileUrl: string }) => {
+                    return (
                 <div
                     key={fileName}
                     className="flex flex-col mb-8 items-start justify-evenly "
@@ -40,11 +75,11 @@ const Skills = () => {
                         </div>
                     </a>
                     <StyledLink
-                        url="https://www.freecodecamp.org/fcc6ebd6462-e213-40c0-8eaf-e06f336c09cd"
-                        title="Free Code Camp Profile"
+                        url={profileUrl}
+                        title={profileTitle}
                     />
                 </div>
-            ))}
+)})}
         </div>
     )
 }

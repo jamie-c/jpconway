798ed43e364d3fcb9e95ed6ec8f5038dfb59ad6c
Jamie Conway <jamie@jpconway.com>
2023-07-17 15:18:03 -0500

    add short project description

--- src/app/(pages)/weather/page.tsx
+++ src/app/(pages)/weather/page.tsx
@@ -1,6 +1,7 @@
 import HeadingOne from "@/components/HeadingOne";
 import WeatherForecast from "@/components/weather/WeatherForecast";
 import { getApiKey } from "./getWeatherForecast";
+import Link from "next/link";
 
 const Weather = () => {
 
@@ -10,6 +11,13 @@ const Weather = () => {
         <div id="weather-forecast" className="px-1 md:px-24 lg:px-48 w-full max-w-7xl">
             <HeadingOne title='Weather App'/>
             <WeatherForecast api_key={WEATHER_API_KEY}/>
+            <div>
+                <h3 className="text-3xl mt-10 mb-5">About the Project</h3>
+                <p>This project was a quick Saturday afternoon project built during one of the OKCoders classes.</p>
+                <p>The goal here was simply to consume an API.</p>
+                <p>I've posted my version here.</p>
+                <p>The API is <Link href="https://www.weatherapi.com/">weatherapi.com</Link></p>
+            </div>
         </div>
     );
 };

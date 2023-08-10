d34627f01b1c82de4b0e4b772286d77089cc564a
Jamie Conway <jamie@jpconway.com>
2023-08-10 15:13:14 -0500

    add api endpoint for individual post data

---
+++ src/pages/api/posts/[id]/index.ts
@@ -0,0 +1,21 @@
+import { getPostData } from '@/lib/posts'
+
+export default async function handler(req: Request, res: Response) {
+    
+    const { id } = req.query
+
+    if (req.method === 'GET') {
+        try {
+            (async () => {
+                const postData = await getPostData(id)
+                res.status(200).json(postData)
+            })()
+
+        } catch (error) {
+            console.log("🚀 ~ file: route.ts:25 ~ GET ~ error", error)
+            res.json({error})
+        }
+    } else {
+        res.json({ message: 'This endpoint only supports GET requests' })
+    }
+}

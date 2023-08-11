4abd775df521f2d83db99c7bb286c44ca7ee51ef
Jamie Conway <jamie@jpconway.com>
2023-08-11 13:13:24 -0500

    remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
 import LandingPage from '@/components/LandingPage'
-import Link from 'next/link'
 
 export default function Home() {
     return (
         <>
             <header></header>
-            <main className='min-h-100 w-100 flex flex-col items-center pb-10'
+            <main className='min-h-100 w-screen flex flex-col items-center pb-10'
             >
                 <LandingPage />
             </main>

db6e88644d9c7cf29f7a2f7831528f5059132328
Jamie Conway <jamie@jpconway.com>
2023-08-10 23:13:47 -0500

    add pdf resume and route

---
+++ src/app/resume/route.js
@@ -0,0 +1,6 @@
+import { NextResponse } from 'next/server';
+
+export function GET() {
+    // Send the PDF content as the response
+    return NextResponse.redirect(`https:jpconway.com/files/resume-for-jamie-conway.pdf`);
+}

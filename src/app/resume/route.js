0e55163f4e299efe82ba4a3ad64f429c0b37353f
Jamie Conway <jamie.c@nodecraft.com>
2024-10-01 07:25:41 -0500

    fix: update resume

--- src/app/resume/route.js
+++ src/app/resume/route.js
@@ -2,5 +2,5 @@ import { NextResponse } from 'next/server';
 
 export function GET() {
     // Send the PDF content as the response
-    return NextResponse.redirect(`https:jpconway.com/files/resume-for-jamie-conway.pdf`);
+    return NextResponse.redirect("https:jpconway.com/files/resume-for-jamie-conway.pdf");
 }
